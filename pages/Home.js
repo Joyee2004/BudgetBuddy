a4c4eb299070a734cef55f91b7c5927b518986a5
Sayali <124203472+say2410@users.noreply.github.com>
2023-11-17 15:40:08 +0530

    Update Home.js

--- pages/Home.js
+++ pages/Home.js
@@ -3,29 +3,40 @@ import './Home.css';
 import Card from "./Card.js";
 
 const Home =()=>{
-return(
-    <>
+return (
+  <>
     <div className="img-wrapper">
-        <img src="/img/home1.webp" alt ="h"  className='back-img'/>
-        
-            <div className='back-text'>
-           <h1>Online Expense Management</h1>
-           <h3>Easily manage your expenses</h3>
-           <button id='signup-btn'>Sign Up for Free</button>
-           
-           </div>      
+      <img src={require("../img/home1.webp")} alt="h" className="back-img" />
+
+      <div className="back-text">
+        <h1 className='name'>Online Expense Management</h1>
+        <h3>Easily manage your expenses</h3>
+        <button id="signup-btn">Sign Up for Free</button>
+      </div>
     </div>
-        
-    <div className='flex-container1'>
-        <Card text1="Simple Money Tracker" text2="orem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed volutpat magn" />
-        <Card text1="Painless Budgeting"   text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus blandit justo id porttitor. Sed ultricies sit amet tortor id gravida. Quisque posuere arcu ut dui blandit, quis pulvinar velit consequat. Mauris in commodo augue, nec facilisis orci. Aenean euismod ex sit amet purus venenatis, ac sodales purus venenatis. Su" />
-     </div>
-     <div className='flex-container1'>
-       <Card text1="Multiple Devices" text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus blandit justo id porttitor. Sed ultricies sit amet tortor id gravida. Quisque posuere arcu ut dui blandit, quis pulvinar velit consequat. Mauris in commodo augue, nec facilisis orci. Aenean euismod ex sit amet purus venenatis, ac sodales purus venenatis. Su" />
-       <Card text1="Recurring Transactions" text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus blandit justo id porttitor. Sed ultricies sit amet tortor id gravida. Quisque posuere arcu ut dui blandit, quis pulvinar velit consequat. Mauris in commodo augue, nec facilisis orci. Aenean euismod ex sit amet purus venenatis, ac sodales purus venenatis. Su" />
-     </div>
-    </>
-)
+
+    <div className="flex-container1">
+      <Card
+        text1="Simple Money Tracker"
+        text2="orem ipsum dolor sit amet, consectetur adipiscing elit. Donec sed volutpat magn"
+      />
+      <Card
+        text1="Painless Budgeting"
+        text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus blandit justo id porttitor. Sed ultricies sit amet tortor id gravida. Quisque posuere arcu ut dui blandit, quis pulvinar velit consequat. Mauris in commodo augue, nec facilisis orci. Aenean euismod ex sit amet purus venenatis, ac sodales purus venenatis. Su"
+      />
+    </div>
+    <div className="flex-container1">
+      <Card
+        text1="Multiple Devices"
+        text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus blandit justo id porttitor. Sed ultricies sit amet tortor id gravida. Quisque posuere arcu ut dui blandit, quis pulvinar velit consequat. Mauris in commodo augue, nec facilisis orci. Aenean euismod ex sit amet purus venenatis, ac sodales purus venenatis. Su"
+      />
+      <Card
+        text1="Recurring Transactions"
+        text2="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus blandit justo id porttitor. Sed ultricies sit amet tortor id gravida. Quisque posuere arcu ut dui blandit, quis pulvinar velit consequat. Mauris in commodo augue, nec facilisis orci. Aenean euismod ex sit amet purus venenatis, ac sodales purus venenatis. Su"
+      />
+    </div>
+  </>
+);
 }
 
 export default Home;
