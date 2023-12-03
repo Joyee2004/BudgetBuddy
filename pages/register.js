09b26695783f2cd54a0fb197dd68eb1c6cabe7b8
Sayali <124203472+say2410@users.noreply.github.com>
2023-12-03 14:25:50 +0530

    Update register.js

--- pages/register.js
+++ pages/register.js
@@ -1,34 +1,60 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Checkbox from "../Components/checkbox.js";
 import "./login.css";
-import image from "../img/login.png";
-const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [name, setName] = useState('');
-    const[username,setUsername]=useState('');
- 
-    const[gusername,setGusername]=useState('');
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(email);
-    }
 
-    return (
-      <div
-        style={{
-          backgroundImage: `url(${image})`,
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-        }}
-        className="App"
-      >
+const Register = (props) => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+
+  const [gusername, setGusername] = useState("");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, email, username, pass }),
+      });
+
+      if (response.ok) {
+        const responseData = await response.json(); // Parse response body as JSON
+        console.log("Register successful:", responseData.message);
+        navigate(responseData.redirect);
+      } else {
+        // Handle login failure
+        const errorData = await response.json(); // Parse error response body as JSON
+        console.error("Register failed:", errorData.message);
+      }
+    } catch (error) {
+      console.error("Error during Register:", error);
+    }
+  };
+
+  return (
+    <div className="completer">
+      <div className="register-all">
+        <div className="register-img">
+          <img
+            src="https://png.pngtree.com/template/20211026/ourmid/pngtree-blue-gradient-cartoon-minimalist-style-login-background-page-image_693616.jpg"
+            alt="login-img"
+          />
+        </div>
         <div className="form-container">
+          <h2 className="headerll">
+            <center>SIGN UP</center>
+          </h2>
           <form className="register-form" onSubmit={handleSubmit}>
-            <h2 className="header">SIGN UP</h2>
             <p className="text">
               {" "}
-              Hey buddy,its time to track and manage your expenses!!
+              <b>
+                <i>Hey buddy,its time to track and manage your expenses!!</i>
+              </b>
             </p>
             <label className="loglabel" htmlFor="name">
               Full name
@@ -43,7 +69,7 @@ const Register = (props) => {
               required
             />
             <label className="loglabel" htmlFor="email">
-              email
+              Email
             </label>
             <input
               className="log"
@@ -56,7 +82,7 @@ const Register = (props) => {
               required
             />
             <label className="loglabel" htmlFor="username">
-              set user name
+              Set user name
             </label>
             <input
               className="log"
@@ -68,7 +94,7 @@ const Register = (props) => {
               required
             />
             <label className="loglabel" htmlFor="password">
-              password
+              Password
             </label>
             <input
               className="log"
@@ -81,32 +107,20 @@ const Register = (props) => {
               required
             />
 
-            <label htmlFor="gusername" className="loglabel">
-              Add guardian username(optional)
-            </label>
-
-            <input
-              value={gusername}
-              name="Guardian username"
-              className="log"
-              onChange={(e) => setGusername(e.target.value)}
-              id="Guardian username"
-              placeholder="guardian user Name"
-            />
-
             <Checkbox label="I agree to terms & conditions" />
+
             <button type="submit" className="sign-up">
               Sign up
             </button>
           </form>
-          <button
-            className="link-btn"
-            onClick={() => props.onFormSwitch("login")}
-          >
-            Already have an account? Login here.
-          </button>
+          <a href="/login">
+            <button className="link-btnr">
+              Already have an account? Login here.
+            </button>
+          </a>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 export default Register;
