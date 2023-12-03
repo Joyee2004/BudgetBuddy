514e0b77c9f6d4d881caca3a45a8cd3a0fa85e85
Sayali <124203472+say2410@users.noreply.github.com>
2023-12-03 14:21:14 +0530

    Update calender.js

--- pages/calender.js
+++ pages/calender.js
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./calendar.css";
+import { useNavigate } from "react-router-dom";
 
-const Calendar = () => {
-  function handleClick() {
-    alert("You clicked me!");
-  }
-
+const Calendar = (props) => {
+  const navigate = useNavigate();
+  const handleClick = async (index) => {
+    console.log(`Clicked on day ${index}`);
+    try {
+      const response = await fetch("http://localhost:3000/calendar", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ index }),
+      });
+      if (response.ok) {
+        const responseData = await response.json(); // Parse response body as JSON
+        console.log("Server Response:", responseData);
+        alert(`Today's total expenses: ${responseData.amount}`);
+      } else {
+        // Handle login failure
+        const errorData = await response.json(); // Parse error response body as JSON
+        console.error("Login failed:", errorData.message);
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+    }
+  };
+const handleAddExpenseClick = () => {
+  // Redirect to the expense page
+  navigate("/expense");
+};
   return (
     <>
-      <center className="header">
+      <center className="headerr">
         <strong>Savings Tracker</strong>
       </center>
 
@@ -17,20 +42,24 @@ const Calendar = () => {
           {Array.from({ length: 31 }, (_, index) => (
             <div className="day" key={index + 1}>
               <p>{index + 1}st</p>
-              <button className="button" onClick={handleClick}>
+              <button className="button" onClick={() => handleClick(index + 1)}>
                 View Expenses
               </button>
               <br />
-              <button className="button1">Add Expenses</button>
+              <button className="button1" onClick={handleAddExpenseClick}>
+                Add Expense
+              </button>
             </div>
           ))}
           <div className="day">
             <p>Total</p>
-            <button className="button" onClick={handleClick}>
+            <button className="button" onClick={() => handleClick("total")}>
               View Expenses
             </button>
             <br />
-            <button className="button1">Add Expenses</button>
+            <button className="button1" onClick={handleAddExpenseClick}>
+              Add Expense
+            </button>
           </div>
         </div>
       </div>
