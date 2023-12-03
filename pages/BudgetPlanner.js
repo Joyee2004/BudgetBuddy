259910c491a74095766cf3fcf294d7be4621db95
Sayali <124203472+say2410@users.noreply.github.com>
2023-12-03 14:19:47 +0530

    Updated BudgetPlanner.js

--- pages/BudgetPlanner.js
+++ pages/BudgetPlanner.js
@@ -1,91 +1,123 @@
-// src/BudgetPlanner.js
-import React, { useState } from 'react';
-import './BudgetPlanner.css'; // Import custom styles
+import React, { useState } from "react";
+import "./BudgetPlanner.css"; // Import custom styles
 
 const BudgetPlanner = () => {
   const [budgetItems, setBudgetItems] = useState([]);
-  const [category, setCategory] = useState('');
-  const [dueDate, setDueDate] = useState('');
-  const [amount, setAmount] = useState('');
-  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [amount, setAmount] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleData = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:3000/budgetplanner", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ category, dueDate, amount, description }),
+      });
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log("Server Response:", responseData);
+        alert('Updated Budget planner!')
+      } else {
+        const errorData = await response.json();
+        console.error("Data submission failed:", errorData.message);
+      }
+    } catch (error) {
+      console.error("Error during data submission:", error);
+    }
+  };
 
   const handleAddBudgetItem = () => {
     if (category && dueDate && amount && description) {
-      setBudgetItems([...budgetItems, { category, dueDate, amount, description }]);
-      setCategory('');
-      setDueDate('');
-      setAmount('');
-      setDescription('');
+      setBudgetItems([
+        ...budgetItems,
+        { category, dueDate, amount, description },
+      ]);
+      setCategory("");
+      setDueDate("");
+      setAmount("");
+      setDescription("");
     } else {
-      alert('Please fill in all fields.');
+      alert("Please fill in all fields.");
     }
   };
 
   return (
-    <div className="budget-planner-container">
-      <div class="heading"><h1><center>BUDGET PLANNER</center></h1></div>
-      <center>
-      <div className="budget-form">
-        <label>Category: </label>
-        <select
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        >
-          <option value="">Select Category</option>
-          <option value="Household bills">Household bills</option>
-          <option value="Living costs">Living costs</option>
-          <option value="Food">Food</option>
-          <option value="Travel">Travel</option>
-          <option value="Leisure">Leisure</option>
-          <option value="Education">Education</option>
-          <option value="Others">Others</option>
-        </select>
-        <br/>
-        <br/>
+    <div className="page-body">
+      <div className="bcontainer">
+        <div className="heading">
+          <h1>
+            <center>BUDGET PLANNER</center>
+          </h1>
+        </div>
+        <center>
+          <div className="budget-form">
+            <label>Category: </label>
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">Select Category</option>
+              <option value="Household bills">Household bills</option>
+              <option value="Medical">Medical</option>
+              <option value="Food">Food</option>
+              <option value="Travel">Travel</option>
+              <option value="Leisure">Leisure</option>
+              <option value="Education">Education</option>
+              <option value="Others">Others</option>
+            </select>
+            <br />
+            <br />
 
-        <label>Due Date:</label>
-        <input
-          type="date"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-        />
+            <label>Due Date:</label>
+            <input
+              type="date"
+              value={dueDate}
+              onChange={(e) => setDueDate(e.target.value)}
+            />
 
-        <br/>
-        <br/>
+            <br />
+            <br />
 
-        <label>Amount Expected:</label>
-        <input
-          type="text"
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
+            <label>Amount Expected:</label>
+            <input
+              type="text"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+            />
 
-        <br/>
-        <br/>
+            <br />
+            <br />
 
-        <label>Description:</label>
-        <input
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <br/>
-        <br/>
-        <button onClick={handleAddBudgetItem}>Add Budget Item</button>
-      </div>
-      </center>
+            <label>Description:</label>
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+            <br />
+            <br />
+            <button onClick={handleAddBudgetItem}>Add Budget Item</button>
+          </div>
+        </center>
 
-      <div className="budget-list">
-        <h2>Budget Items:</h2>
-        <ul>
-          {budgetItems.map((item, index) => (
-            <li key={index}>
-              <strong>Category: </strong> {item.category},<br/> <strong>Due Date:</strong>{' '}
-              {item.dueDate},<br/> <strong>Amount Expected:</strong> {item.amount},<br/>{' '}
-              <strong>Description:</strong> {item.description}
-            </li>
-          ))}
-        </ul>
+        <div className="budget-list">
+          <h2>Budget Items:</h2>
+          <ul>
+            {budgetItems.map((item, index) => (
+              <li key={index}>
+                <strong>Category: </strong> {item.category},<br />{" "}
+                <strong>Due Date:</strong> {item.dueDate},<br />{" "}
+                <strong>Amount Expected:</strong> {item.amount},<br />{" "}
+                <strong>Description:</strong> {item.description}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
